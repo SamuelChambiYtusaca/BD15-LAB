fc338890f3c1384ab88a2c82fc9c023d79448c56
SamuelChambiYtusaca <82793718+SamuelChambiYtusaca@users.noreply.github.com>
2021-12-09 23:03:47 -0500

    Delete routes directory

--- routes/publi.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {create,list} = require("../controllers/controller.js")
-
-router.post('/create',create)
-router.get('/list',list)
-
-module.exports = router;
