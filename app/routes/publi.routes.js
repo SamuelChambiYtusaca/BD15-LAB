c9e50da85e5cd75f0377f3c54983a68e2ff66191
SamuelChambiYtusaca <82793718+SamuelChambiYtusaca@users.noreply.github.com>
2021-12-10 12:59:48 -0500

    Delete app directory

--- app/routes/publi.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const publis = require("../controllers/publi.controller.js");
-    var router = require("express").Router();
-    router.post("/", publis.create);
-    router.get("/", publis.findAll);
-    app.use('/api/publicaciones', router);
-  };
